b02801242993006161338434b7815f4caab9bfbb
xuchuanliang <xuchuanliang361@163.com>
2019-05-14 17:01:33 +0800

    init

---
+++ src/test/java/com/snail/javadesign/JavaDesignApplicationTests.java
@@ -0,0 +1,16 @@
+package com.snail.javadesign;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaDesignApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
